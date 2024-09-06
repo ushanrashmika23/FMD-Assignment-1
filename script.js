fefa533c50685bd2b147be0004375249f96c01aa
Ushan Rashmika <63453670+ushanrashmika23@users.noreply.github.com>
2024-09-06 20:00:21 +0530

    project done. front end != responsive

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let header=document.querySelector('header');
+let header = document.querySelector('header');
 document.addEventListener(
     'scroll',
     () => {
         if (window.scrollY > 0) {
             console.log(window.scrollY);
             header.classList.add("scrolled");
-        }else{
+        } else {
             header.classList.remove("scrolled");
         }
     }
 );
+
+function active(cls) {
+
+    let btns = document.querySelectorAll('#btn1, #btn2, #btn3, #btn4');
+
+    if (btns[cls].classList.contains('btn-on')) {
+        btns[cls].classList.remove('btn-on');
+    } else {
+        btns[cls].classList.add('btn-on');
+    }
+
+
+}
